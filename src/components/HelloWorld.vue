dcdb3ba7eb979e326976fc4f9b73a48b5a978934
rodulph.huang <rudolph.huang@tron.network>
2022-04-26 21:54:18 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="flex flex-col min-h-screen overflow-hidden scroll-smooth hover:scroll-auto bg-gray-100">
+    <section class="relative max-w-6xl mx-auto px-4 sm:px-6 m-20 bg-white p-6 shadow-lg	rounded-md" id="navigation">
+
+      <div class="m-5">
+        Proxy Contract Address:
+        <el-input placeholder="0x.." v-model="proxyContractAddress"/>
+      </div>
+
+      <div class="m-5">
+        Logic Contract Address:
+        <el-input placeholder="0x.." v-model="logicContractAddress"/>
+      </div>
+
+      <div class="m-5">
+        <el-button type="primary" @click="handleClick">
+          Submit
+        </el-button>
+      </div>
+
+    </section>
+
     <section class="relative max-w-6xl mx-auto px-4 sm:px-6 m-20 bg-white p-6 shadow-lg	rounded-md" id="home">
 
+      <div>
+        <el-button type="primary" @click="read">Read as proxy</el-button>
+        <el-button type="primary" @click="white">Write as proxy</el-button>
+      </div>
+
+      <div class="m-5" v-if="type==='read'">
+        <div v-for="item in read_functions" :key="item.name">
+          <span class="font-bold">{{ item.name }}</span>
+        </div>
+      </div>
 
     </section>
   </div>
@@ -9,10 +39,17 @@
 
 
 <script>
-// import {ref} from 'vue'
+import {ref} from 'vue'
 // import {ElNotification} from 'element-plus'
 // import axios from "axios";
 
+const proxyContractAddress = ref('TJzar3ayukrPcHDY7py7VYmkoeKvJ7sfwC');
+const logicContractAddress = ref('TRPqQYtqtvW8XNuxctLxGgSDTuSUDX3GeJ');
+
+const read_functions = ref([]);
+const write_functions = ref([]);
+
+const type = ref('read');
 
 export default {
   name: 'HelloWorld',
@@ -22,7 +59,44 @@ export default {
   components: {},
   setup() {
     return {
+      type: type,
+      read_functions,
+      write_functions,
+      proxyContractAddress: proxyContractAddress,
+      logicContractAddress: logicContractAddress,
+      read: async () => {
+        type.value = 'read';
+      },
+      white: async () => {
+        type.value = 'write';
+      },
+      handleClick: async () => {
 
+        let logic_contract = await window.tronWeb.contract().at(logicContractAddress.value);
+        let proxy_contract = await window.tronWeb.contract().at(proxyContractAddress.value);
+
+        // console.log('click', logicContractAddress, proxyContractAddress);
+        console.log('logic', logic_contract);
+        console.log('proxy', proxy_contract);
+
+        const logic_abi = logic_contract.abi;
+
+        for (let i = 0; i < logic_abi.length; i++) {
+          if (logic_abi[i].type === 'function') {
+            // read_functions.value.push(logic_abi[i]);
+            // console.log(logic_abi[i].stateMutability);
+
+            if(logic_abi[i].stateMutability === 'view' || logic_abi[i].stateMutability === 'pure') {
+              read_functions.value.push(logic_abi[i]);
+            }else{
+              write_functions.value.push(logic_abi[i]);
+            }
+
+          }
+          // console.log(logic_abi[i]);
+        }
+
+      }
     }
   }
 }
